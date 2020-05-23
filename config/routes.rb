fceb8ab0b6d762805675cc37d6a531d7e5dd7f26
Ikhsan Maulana <ixandidu@gmail.com>
2020-05-23 20:04:59 +0700

    Upgrade Rails {5.2.4.3 -> 6.0.3.1}

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'leaderboard/users'
   get 'leaderboard/projects'
   root 'leaderboard#users'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
